35810e3f10531873cca103f4e7cccaa396e147a4
Charmaine Dobbin <19003337@vcconnect.co.za>
2021-03-22 11:11:53 +0200

    Initial commit

---
+++ app/src/main/java/com/vc19003337/myweatherapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vc19003337.myweatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
